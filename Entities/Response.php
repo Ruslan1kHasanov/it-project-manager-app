a7fbe8ac1beb2dab822cfe31d142f694ac0fca1c
Ruslan kHasanov <khazaruscsu@gmail.com>
2023-05-03 22:48:46 +0500

    modify contribibotors adding

--- Entities/Response.php
+++ Entities/Response.php
@@ -1,11 +1,15 @@
 <?php
 
-class Response {
+class Response
+{
     public bool $error;
     public string $message;
+    public string $raw_data;
 
-    function __construct(bool $error, string $message) {
+    function __construct(bool $error, string $message, string $raw_data = "")
+    {
         $this->error = $error;
         $this->message = $message;
+        $this->raw_data = $raw_data;
     }
 }
--- proj_server.php
+++ proj_server.php
@@ -166,7 +166,7 @@ function create_new_column($conf, $data)
     }
 }
 
-function is_user_exists($conf, $user_email)
+function is_user_exists($conf, $user_email): bool
 {
     $pdo = new PDO("mysql:host=$conf->host;dbname=$conf->db;cahrset=$conf->charset", $conf->user, $conf->password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -180,20 +180,35 @@ function is_user_exists($conf, $user_email)
     $requested_data = $requested_query->fetch(PDO::FETCH_ASSOC);
     return isset($requested_data['email']);
 }
-function add_contributor($conf, $data){
+
+function select_user_login_email($conf, $user_email)
+{
+    $pdo = new PDO("mysql:host=$conf->host;dbname=$conf->db;cahrset=$conf->charset", $conf->user, $conf->password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $requested_query = $pdo->prepare('
+        select email, login from Users
+        where email = ?;
+    ');
+    $requested_query->execute([$user_email]);
+    return $requested_query->fetch(PDO::FETCH_ASSOC);
+}
+
+function add_contributor($conf, $data): Response
+{
     try {
         $pdo = new PDO("mysql:host=$conf->host;dbname=$conf->db;cahrset=$conf->charset", $conf->user, $conf->password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        if (is_user_exists($conf, $data['contributor_email'])){
+        if (is_user_exists($conf, $data['contributor_email'])) {
             $requested_query = $pdo->prepare('
                 insert into Projects_list
                 values(?, ?, ?);
             ');
             $requested_query->execute([$data['proj_id'], $data['contributor_email'], $data['is_admin']]);
 
-            return new Response(true, "USER_WAS_INVITED");
-        }else{
+            return new Response(false, "USER_WAS_INVITED",json_encode(select_user_login_email($conf, $data['contributor_email'])));
+        } else {
             return new Response(true, "USER_IS_NOT_EXIST");
         }
     } catch (Exception $e) {
@@ -221,33 +236,39 @@ function add_contributor($conf, $data){
 //    echo json_encode(new Response(true, MUST_BE_AUTHORIZED));
 //}
 
-switch ($data['type']){
-    case GET_PROJECT_LIST: {
+switch ($data['type']) {
+    case GET_PROJECT_LIST:
+    {
         http_response_code(200);
         $response = get_project_list($conf, $data);
         break;
     }
-    case CREATE_NEW_PROJECT: {
+    case CREATE_NEW_PROJECT:
+    {
         http_response_code(201);
         $response = create_new_project($conf, $data);
         break;
     }
-    case GET_PROJECT_DATA: {
+    case GET_PROJECT_DATA:
+    {
         http_response_code(200);
         $response = get_project_data($conf, $data);
         break;
     }
-    case CREATE_NEW_COLUMN: {
+    case CREATE_NEW_COLUMN:
+    {
         http_response_code(201);
         $response = create_new_column($conf, $data);
         break;
     }
-    case INVITE_CONTRIBUTOR: {
+    case INVITE_CONTRIBUTOR:
+    {
         http_response_code(201);
         $response = add_contributor($conf, $data);
         break;
     }
-    default:{
+    default:
+    {
         http_response_code(400);
         $response = new Response(true, "BAD_REQUEST_TO_SERVER");
         break;
